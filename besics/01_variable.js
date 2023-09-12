521b17e331b838a766e9ac217daaf9a560715536
jajati <144675536+jajati95@users.noreply.github.com>
2023-09-12 19:12:35 +0000

    besics

---
+++ besics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "jayanshidairydream@gmail.com"
+var accountPassword = "199595"
+accountCity = "bangalore"
+let accountState;
+
+// accountId = 2345
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/* notes- preser not to use var because of issue in block scope and functional scope */
