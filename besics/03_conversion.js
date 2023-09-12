7e506f69e2e0397196e7decdd626e7fcb73a4b94
jajati <144675536+jajati95@users.noreply.github.com>
2023-09-12 19:50:11 +0000

    conversion

---
+++ besics/03_conversion.js
@@ -0,0 +1,29 @@
+let score = "jajati"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+
+
+
+//"33" => 33
+//"33abc" =>NaN(not a number)
+//true => 1; false =>0
+
+
+let isLoggedIn = "jajati"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(bolleanIsLoggedIn);
+
+//1 => true; 0=> false
+//"" => false
+// "jajati" => true
+
+let someNumber = 99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
