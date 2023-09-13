8fc3f4ccf7693434c7df09b85fe573caa48007ea
jajati <144675536+jajati95@users.noreply.github.com>
2023-09-13 12:20:22 +0000

    conversion

--- besics/03_conversion.js
+++ besics/03_conversion.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "jajati"
 
 let bolleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(bolleanIsLoggedIn);
+//console.log(bolleanIsLoggedIn);
 
 //1 => true; 0=> false
 //"" => false
@@ -25,5 +25,36 @@ console.log(bolleanIsLoggedIn);
 let someNumber = 99
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**************Operation********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "jajati"
+let str2 = " hello"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let gemcounter= 100
+++gemcounter
+console.log(gemcounter);
+
+// note for study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
